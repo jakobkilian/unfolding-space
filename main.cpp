a2eeb6b8272a7a087430f75f73b982410dc8caf3
janostondar <jakobkilian.de>
2019-06-27 17:09:18 +0200

    udpx3

--- main.cpp
+++ main.cpp
@@ -43,10 +43,7 @@ bool gui=0;
 
 using boost::asio::ip::udp;
 boost::asio::io_service io_service;
-
-
 udp::socket myInputSocket(io_service, udp::endpoint(udp::v4(), 52222));
-
 udp::resolver resolver(io_service);
 udp::resolver::query query(udp::v4(), argv[1], "daytime");
 udp::endpoint receiver_endpoint = *resolver.resolve(query);
